6d2f2c57463e2a39d689a8672f2a28ac441075fd
ricardo <a.ricardohg@gmail.com>
2013-12-24 13:04:47 -0500

    inital project

---
+++ Disclient/Disclient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Disclient
+//
+//  Created by ricardo hernandez on 12/24/13.
+//  Copyright (c) 2013 ric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
