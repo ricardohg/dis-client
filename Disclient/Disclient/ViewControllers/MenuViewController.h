e8fe998238f1b8241d21ffd9c5330ad40e18a2f4
ricardo <a.ricardohg@gmail.com>
2014-03-02 16:26:52 -0500

    added drawer controller, svprogresshud

---
+++ Disclient/Disclient/ViewControllers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Disclient
+//
+//  Created by ricardo hernandez on 3/2/14.
+//  Copyright (c) 2014 ric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
