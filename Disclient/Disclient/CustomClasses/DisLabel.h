3706cbeee0a29e99221419861b2da94174fbc1b3
ricardo <a.ricardohg@gmail.com>
2014-07-18 16:14:56 -0500

    added custom label class

---
+++ Disclient/Disclient/CustomClasses/DisLabel.h
@@ -0,0 +1,25 @@
+//
+//  DisLabel.h
+//  Disclient
+//
+//  Created by ricardo hernandez on 7/18/14.
+//  Copyright (c) 2014 ric. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DisLabel : UILabel
+
+@end
+
+@interface DisButton : UIButton
+
+@end
+
+@interface DisTextField : UITextField
+
+@end
+
+@interface disTextView : UITextView
+
+@end
